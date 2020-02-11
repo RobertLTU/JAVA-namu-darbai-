da0cddccb150211a533d8f44807a7f0bd6246ff6
RobertLTU <58054709+RobertLTU@users.noreply.github.com>
2020-02-11 23:48:57 +0200

    Create homework1ex10.java

---
+++ namu darbas 1/homework1ex10.java
@@ -0,0 +1,10 @@
+/* HW 1.10*/
+
+class homework1ex10
+{
+public static void main(String[] args)
+	{
+	double rezultatas = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+	System.out.println(rezultatas);
+	}
+}
