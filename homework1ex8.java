e4b75ff9fa10307772b386e99f9e4692879b3e0b
RobertLTU <58054709+RobertLTU@users.noreply.github.com>
2020-02-11 16:26:57 +0200

    Add files via upload
    1 paskaitos namu darbai is w3 tinklapio

---
+++ homework1ex8.java
@@ -0,0 +1,11 @@
+/* HW 1.8 Nupiesti uzrasha JAVA*/
+
+class homework1ex8
+{
+public static void main(String[] args)
+	{
+	System.out.println(
+	"   J    a   v     v  a\n   J   a a   v   v  a a\nJ  J  aaaaa   V V  aaaaa\n JJ  a     a   V  a     a" 
+	);
+	}
+}
