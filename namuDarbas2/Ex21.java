32a956699617a2aba8be4963e786f8d4d75d4998
RobertLTU <58054709+RobertLTU@users.noreply.github.com>
2020-02-13 20:29:17 +0200

    Create Ex21.java

---
+++ namuDarbas2/Ex21.java
@@ -0,0 +1,21 @@
+/* Homework_2 excercise_1
+Write a Java program to get a number from the user and print whether it is positive or negative.
+Test Data
+Input number: 35
+Expected Output :
+Number is positive
+*/
+
+public class Ex21 {
+	public static void main (String[] args) {
+	System.out.println("Please enter your number :"); 
+	int userNumber = Integer.valueOf(System.console().readLine());
+	if (userNumber < 0) {
+		System.out.println("Number is negative");
+	} else if (userNumber > 0) {
+	System.out.println("Number is positive");
+	} else {
+	System.out.println("Your number is ZERO!");
+	}
+	}
+}
