05e9e181061d369c8ae64c64a4e939538b510392
RobertLTU <58054709+RobertLTU@users.noreply.github.com>
2020-02-13 16:48:11 +0200

    Update Uzduotis1.java

--- namu darbas 3/Uzduotis1.java
+++ namu darbas 3/Uzduotis1.java
@@ -1,15 +1,14 @@
 /*
-1. Ar dudejanti seka ar mazejanti ? Ar isvis jokios sekos ? 
-2. Atspausdinti skaicius atvirkstine seka.
-{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+1 uzduotis
 */
+
 public class Uzduotis1
 {
 	public static void main(String[] args) 
 	{
 		int[] seka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 		int sekosTipas = 0;
+		System.out.println("Sekos ilgis :" + seka.length);
 	for (int i = 0; i < (seka.length - 1); i++)
 	{
 		if (seka[i] < seka[i + 1] ) {
@@ -30,7 +29,6 @@ public class Uzduotis1
 		// Atvirkstines sekos isvedimas OUT
 		for (int j = 10; j >= 0; j--) {
 			System.out.println(seka[j]);
-			// String atvirkstineSeka = "";
 		}
 	}
 }
