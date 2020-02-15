7658d8b86f1c8b2e3fc70eae788af2015eaa59e6
RobertLTU <58054709+RobertLTU@users.noreply.github.com>
2020-02-15 22:56:47 +0200

    Update Uzduotis3.java

--- namuDarbas3/Uzduotis3.java
+++ namuDarbas3/Uzduotis3.java
@@ -1,24 +1,31 @@
 public class Uzduotis3 {
 
     public static void main(String[] args) {
-        double a = 0;
+ double a = 0;
         double b = 0;
-        System.out.println("Enter number a :");
-        a = Double.valueOf(System.console().readLine());
-        System.out.println("Enter number b :");
-        b = Double.valueOf(System.console().readLine());
+        String contBreak;
+        String stopCalc = "n";
+        do {
+            System.out.println("Enter number a :");
+            a = Double.valueOf(System.console().readLine());
+            System.out.println("Enter number b :");
+            b = Double.valueOf(System.console().readLine());
 
-//      KODEL NESUVEIKIA SITAS VARIANTAS su parseDouble???
+//      KODEL NESUVEIKIA SITAS VARIANTAS ???
 //        System.out.println("Enter number a :");
 //        a = Double.parseDouble(System.console().readLine())
-        
+
 //        System.out.println("Enter number b :");
 //        b = Double.parseDouble(System.console().readLine())
 
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("a / b = " + (a / b));
-        System.out.println("a % b = " + (a % b));
+            System.out.println("a + b = " + (a + b));
+            System.out.println("a - b = " + (a - b));
+            System.out.println("a * b = " + (a * b));
+            System.out.println("a / b = " + (a / b));
+            System.out.println("a % b = " + (a % b));
+            System.out.println("Press \"n\" if you wish to stop the calculation");
+            contBreak = System.console().readLine();
+            
+        } while (!contBreak.equals("n"));
     }
 }
